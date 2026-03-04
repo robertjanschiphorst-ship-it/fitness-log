9c0a0291fb235f97807ddea45a5f4ee4ac376c70
R.J.F. Schiphorst <robert-jan@Mac.home>
2026-03-04 10:21:21 +0100

    Fix prisma config type error

--- prisma.config.ts
+++ prisma.config.ts
@@ -15,8 +15,6 @@ try {
   }
 } catch {}
 
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
@@ -25,11 +23,5 @@ export default defineConfig({
   },
   datasource: {
     url: process.env.TURSO_DATABASE_URL!,
-    adapter: () => {
-      const url = process.env.TURSO_DATABASE_URL;
-      const authToken = process.env.TURSO_AUTH_TOKEN;
-      if (!url) throw new Error("TURSO_DATABASE_URL is not set");
-      return new PrismaLibSql({ url, authToken });
-    },
   },
 });
